d67b9680b23bcb4ca00b2dba4aec1aac7ecee480
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2023-09-21 14:14:03 -0700

    Update problems.js

--- problems.js
+++ problems.js
@@ -1,1 +1,5 @@
+// Write a JavaScript function that reverses a number. Test your code.
+
+
+// Write a JavaScript function that checks whether a passed string is a palindrome or not. Text your code.
 
